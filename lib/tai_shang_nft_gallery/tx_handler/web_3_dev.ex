36dff0bd5c0a41892f17ca4b215aed410fc152d7
leeduckgo <albertschr@163.com>
2022-02-22 07:42:01 +0800

    fix: update uri when set_token_info

--- lib/tai_shang_nft_gallery/tx_handler/web_3_dev.ex
+++ lib/tai_shang_nft_gallery/tx_handler/web_3_dev.ex
@@ -47,12 +47,13 @@ defmodule TaiShangNftGallery.TxHandler.Web3Dev do
     "setTokenInfo",
     %{id: nft_c_id}, _from, _to, _value,
     [token_id, badges_raw], _chain) do
-    # UPDATE TokenInfo
+    # UPDATE Badges & URI
+    uri = Interactor.get_token_uri(chain, addr, token_id)
 
     token_id
     |> Nft.get_by_token_id_and_nft_contract_id(nft_c_id)
     |> Nft.update(%{
-        badges: Poison.decode!(badges_raw), token_id: token_id
+        badges: Poison.decode!(badges_raw), uri: uri
     }, :with_badges)
   end
   def do_handle_tx(_others, _, _, _, _, _, _) do
